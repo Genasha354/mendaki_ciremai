3eabd35fbb9a22bcf1a5b7c1fdba728ef57f0524
Genasha354 <genashaadytar@gmail.com>
2024-06-27 10:21:11 +0700

    Add files via upload

---
+++ asset/Data GeoJSON/POI_Jalur_Palutungan.js
@@ -0,0 +1,331 @@
+var POI_Jalur_Palutungan = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.1",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.433161,
+                    -6.939399,
+                    1221.450562
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Basecamp",
+                "Bujur": "108.433161",
+                "Lintang": "-6.939399",
+                "Ketinggian (MDPL)": "1221.450562\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/pos 1.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.2",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.409939,
+                    -6.934752,
+                    1647.116211
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 1",
+                "Bujur": "108.409939",
+                "Lintang": "-6.934752",
+                "Ketinggian (MDPL)": "1647.116211\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/pos 1.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.3",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.409,
+                    -6.934007,
+                    1595.0
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Mata Air",
+                "Bujur": "108.409",
+                "Lintang": "-6.934007",
+                "Ketinggian (MDPL)": "1595.0\n",
+                "Simbologi": "Sumber Air",
+                "Gambar": "asset/img/Palutungan/pos 1.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.4",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.407453,
+                    -6.932608,
+                    1710.224243
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 2",
+                "Bujur": "108.407453",
+                "Lintang": "-6.932608",
+                "Ketinggian (MDPL)": "1710.224243\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/pos 2.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.5",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.405087,
+                    -6.930552,
+                    1791.907471
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pohon Tumbang 1",
+                "Bujur": "108.405087",
+                "Lintang": "-6.930552",
+                "Ketinggian (MDPL)": "1791.907471\n",
+                "Simbologi": "Waypoint"
+                ,
+                "Gambar": "asset/img/Palutungan/pohon tumbang 1.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.6",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.403102,
+                    -6.925952,
+                    1876.610718
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 3",
+                "Bujur": "108.403102",
+                "Lintang": "-6.925952",
+                "Ketinggian (MDPL)": "1876.610718\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/pos 3.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.7",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.400517,
+                    -6.920099,
+                    2076.861328
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 4",
+                "Bujur": "108.400517",
+                "Lintang": "-6.920099",
+                "Ketinggian (MDPL)": "2076.861328\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/pos4.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.8",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.40009,
+                    -6.918296,
+                    2128.824707
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pohon Tumbang 2",
+                "Bujur": "108.40009",
+                "Lintang": "-6.918296",
+                "Ketinggian (MDPL)": "2128.824707\n",
+                "Simbologi": "Waypoint",
+                "Gambar": "asset/img/Palutungan/pohon tumbang 2.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.9",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.399754,
+                    -6.916743,
+                    2177.059326
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 5",
+                "Bujur": "108.399754",
+                "Lintang": "-6.916743",
+                "Ketinggian (MDPL)": "2177.059326\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/Pos 5.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.10",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.400392,
+                    -6.910979,
+                    2361.345947
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 6",
+                "Bujur": "108.400392",
+                "Lintang": "-6.910979",
+                "Ketinggian (MDPL)": "2361.345947\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/Pos 6.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.11",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.400312,
+                    -6.904704,
+                    2571.109131
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Batas Vegetasi",
+                "Bujur": "108.400312",
+                "Lintang": "-6.904704",
+                "Ketinggian (MDPL)": "2571.109131\n",
+                "Simbologi": "Waypoint",
+                "Gambar": "asset/img/Palutungan/batas vegetasi.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.12",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.400428,
+                    -6.903846,
+                    2607.011719
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Pos 7",
+                "Bujur": "108.400428",
+                "Lintang": "-6.903846",
+                "Ketinggian (MDPL)": "2607.011719\n",
+                "Simbologi": "Pos",
+                "Gambar": "asset/img/Palutungan/pos 7.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.13",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.402192,
+                    -6.899876,
+                    2836.717773
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Persimpangan Apuy - Palutungan",
+                "Bujur": "108.402192",
+                "Lintang": "-6.899876",
+                "Ketinggian (MDPL)": "2836.717773\n",
+                "Simbologi": "Waypoint",
+                "Gambar": "asset/img/Palutungan/persimpangan apuy dan palutungan.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.14",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.40338,
+                    -6.898554,
+                    2924.87915
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Goa Walet",
+                "Bujur": "108.40338",
+                "Lintang": "-6.898554",
+                "Ketinggian (MDPL)": "2924.87915\n",
+                "Simbologi": "Waypoint",
+                "Gambar": "asset/img/Palutungan/goa walet.jpg"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "POI_Palutungan.15",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    108.404976,
+                    -6.897457,
+                    3057.514648
+                ]
+            },
+            "geometry_name": "geom",
+            "properties": {
+                "Nama_Lokasi": "Puncak Via Apuy - Palutungan",
+                "Bujur": "117.80394",
+                "Lintang": "-8.18591\n",
+                "Ketinggian (MDPL)": "3057.514648\n",
+                "Simbologi": "Puncak",
+                "Gambar": "asset/img/Palutungan/puncak.jpg"
+            }
+        },
+    ],
+    "totalFeatures": 15,
+    "numberMatched": 15,
+    "numberReturned": 15,
+    "timeStamp": "2023-10-11T08:38:58.952Z",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:EPSG::4326"
+        }
+    }
+}
