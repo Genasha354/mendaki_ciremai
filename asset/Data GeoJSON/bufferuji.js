0391854bf5f723be45007e38e13091154f81fd30
Genasha354 <genashaadytar@gmail.com>
2024-07-15 11:20:45 +0700

    Add files via upload

---
+++ asset/Data GeoJSON/bufferuji.js
@@ -0,0 +1,8 @@
+var bufferuji = {
+"type": "FeatureCollection",
+"name": "bufferuji",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 105.666832237545535, -5.759481858142912 ], [ 114.532383170091762, -6.547187603020712 ], [ 114.455157116672368, -8.724962309447571 ], [ 106.238305032849041, -8.230715567563461 ], [ 105.666832237545535, -5.759481858142912 ] ] ] ] } }
+]
+}
